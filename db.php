4f8cf320e617e58e33a813ef050f7536bee6eac0
rifmxn <155644211+rifmxn@users.noreply.github.com>
2025-03-19 00:39:26 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,12 @@
 <?php
-$host = 'sql12.freesqldatabase.com';
-$db = 'sql12767940';
-$user = 'sql12767940';
-$pass = 'baI1tCBIgr';
-
+$host = 'localhost';
+$db = 'user_auth';
+$user = 'root';
+$pass = '';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-
-    echo "<h1>Berjaya</h1>";
 }
 ?>
